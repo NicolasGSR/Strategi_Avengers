f0d8ba20156c986f3c1b7c8a6868102b1190909d
Nicolas Reis <nicolasgosore@gmail.com>
2023-01-22 13:51:45 -0300

    criada conexão com db no render

--- config.py
+++ config.py
@@ -7,11 +7,4 @@ marvel = Marvel(PUBLIC_KEY=PUBLIC_KEY, PRIVATE_KEY=PRIVATE_KEY)
 
 SECRET_KEY = 'strategi_avengers'
 
-SQLALCHEMY_DATABASE_URI = \
-    '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
-        SGBD = 'mysql+mysqlconnector',
-        usuario = 'nicolassreis',
-        senha = 'senhauser',
-        servidor = 'localhost',
-        database = 'marvel_db'
-    )
+SQLALCHEMY_DATABASE_URI = "postgresql://projeto_strategi_db_user:5AIzRLRsaCBvpGfSv8Bsf2Nk9Ecd8ct9@dpg-cf6ljj1mbjsmchf5vneg-a.oregon-postgres.render.com/projeto_strategi_db"
--- marvel_db.py
+++ marvel_db.py
@@ -1,71 +1,73 @@
-"""Script para criação do banco de dados necessario"""
+"""Script para criação do banco de dados necessario para testes no localhost
+DEPRECIADO, APP CONECTADO A UM DB HOSTEADO NO RENDER
+"""
 
-import mysql.connector
-from mysql.connector import errorcode
+#import mysql.connector
+#from mysql.connector import errorcode
 
-print("Conectando...")
-try:
-      conn = mysql.connector.connect(
-            host='192.168.0.85',
-            user='nicolassreis',
-            password='senhauser'
-      )
-except mysql.connector.Error as err:
-      if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print('Existe algo errado no nome de usuário ou senha')
-      else:
-            print(err)
+#print("Conectando...")
+#try:
+#      conn = mysql.connector.connect(
+#            host='192.168.0.85',
+#            user='nicolassreis',
+#            password='senhauser'
+#      )
+#except mysql.connector.Error as err:
+#      if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+#            print('Existe algo errado no nome de usuário ou senha')
+#      else:
+#            print(err)
 
-cursor = conn.cursor()
+#cursor = conn.cursor()
 
-cursor.execute("DROP DATABASE IF EXISTS `marvel_db`;")
+#cursor.execute("DROP DATABASE IF EXISTS `marvel_db`;")
 
-cursor.execute("CREATE DATABASE `marvel_db`;")
+#cursor.execute("CREATE DATABASE `marvel_db`;")
 
-cursor.execute("USE `marvel_db`;")
+#cursor.execute("USE `marvel_db`;")
 
-TABLES = {}
-TABLES['Selecao'] = ('''
-      CREATE TABLE `selecao` (
-      `id` int(11) NOT NULL AUTO_INCREMENT,
-      `picture` varchar(400),
-      `name` varchar(50) NOT NULL,
-      `description` varchar(500),
-      PRIMARY KEY (`id`)
-      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;''')
+#TABLES = {}
+#TABLES['Selecao'] = ('''
+#      CREATE TABLE `selecao` (
+#      `id` int(11) NOT NULL AUTO_INCREMENT,
+#      `picture` varchar(400),
+#     `name` varchar(50) NOT NULL,
+#      `description` varchar(500),
+#      PRIMARY KEY (`id`)
+#      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;''')
 
-TABLES['Vingadores'] = ('''
-      CREATE TABLE `vingadores` (
-      `id` int(11) NOT NULL AUTO_INCREMENT,
-      `picture` varchar(400),
-      `name` varchar(50) NOT NULL,
-      `description` varchar(500),
-      PRIMARY KEY (`id`)
-      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;''')
+#TABLES['Vingadores'] = ('''
+#      CREATE TABLE `vingadores` (
+#      `id` int(11) NOT NULL AUTO_INCREMENT,
+#      `picture` varchar(400),
+#      `name` varchar(50) NOT NULL,
+#      `description` varchar(500),
+#      PRIMARY KEY (`id`)
+#      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;''')
 
-TABLES['Equipe'] = ('''
-      CREATE TABLE `equipe` (
-      `id` int(11) NOT NULL AUTO_INCREMENT,
-      `picture` varchar(400),
-      `name` varchar(50) NOT NULL,
-      `description` varchar(500),
-      PRIMARY KEY (`id`)
-      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;''')
+#TABLES['Equipe'] = ('''
+#      CREATE TABLE `equipe` (
+#      `id` int(11) NOT NULL AUTO_INCREMENT,
+#      `picture` varchar(400),
+#      `name` varchar(50) NOT NULL,
+#      `description` varchar(500),
+#      PRIMARY KEY (`id`)
+#      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;''')
 
-for tabela_nome in TABLES:
-      tabela_sql = TABLES[tabela_nome]
-      try:
-            print('Criando tabela {}:'.format(tabela_nome), end=' ')
-            cursor.execute(tabela_sql)
-      except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
-                  print('Já existe')
-            else:
-                  print(err.msg)
-      else:
-            print('OK')
+#for tabela_nome in TABLES:
+#      tabela_sql = TABLES[tabela_nome]
+#      try:
+#            print('Criando tabela {}:'.format(tabela_nome), end=' ')
+#            cursor.execute(tabela_sql)
+#      except mysql.connector.Error as err:
+#            if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+#                  print('Já existe')
+#            else:
+#                  print(err.msg)
+#      else:
+#            print('OK')
 
-conn.commit()
+#conn.commit()
 
-cursor.close()
-conn.close()
+#cursor.close()
+#conn.close()
